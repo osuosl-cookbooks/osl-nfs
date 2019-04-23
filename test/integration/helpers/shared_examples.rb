1ae03fac00e44155b75a3feee4e26f0da682fc7f
Khuong Luu <6059347+khashf@users.noreply.github.com>
2019-04-23 16:01:17 -0700

    Include shared_example.rb in other tests

---
+++ test/integration/helpers/shared_examples.rb
@@ -0,0 +1,32 @@
+if os[:release].to_i < 7
+  describe service('nfs') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+else
+  describe service('nfs') do
+    # Only check enabled on CentOS 7 since this service just runs and exits
+    it { should be_enabled }
+  end
+  describe service('nfs-mountd') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+  describe service('nfs-idmapd') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
+describe command('rpcinfo -p localhost') do
+  [
+    %w(111 portmapper),
+    %w(32765 status),
+    %w(32767 mountd),
+    %w(2049 nfs),
+    %w(2049 nfs_acl),
+    %w(32768 nlockmgr),
+  ].each do |port, service|
+    its(:stdout) { should match(/tcp.*#{port}.*#{service}$/) }
+    its(:stdout) { should match(/udp.*#{port}.*#{service}$/) }
+  end
+end
--- test/integration/homes/inspec/homes_spec.rb
+++ test/integration/homes/inspec/homes_spec.rb
@@ -1,9 +1,3 @@
-require 'shared_examples'
-
-describe 'default' do
-  it_behaves_like 'nfs'
-end
-
 describe file('/etc/exports') do
   its(:content) do
     should eq("/data/homes/ 10.162.136.0/24(rw,sync,mountpoint,no_root_squash)\n")
