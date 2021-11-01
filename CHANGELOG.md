1b46b5fead6070b0b90c35a8424314e209c35ef1
Manatee <github@osuosl.org>
2021-11-01 23:36:36 +0000

    Automatic minor-level version bump to v2.1.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ osl-nfs CHANGELOG
 This file is used to list changes made in each version of the
 osl-nfs cookbook.
 
+2.1.0 (2021-11-01)
+------------------
+- Update to Chef 17
+
 2.0.0 (2021-05-25)
 ------------------
 - Update to new osl-firewall cookbook
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ source_url       'https://github.com/osuosl-cookbooks/osl-nfs'
 license          'Apache-2.0'
 chef_version     '>= 16.0'
 description      'Installs/Configures osl-nfs'
-version          '2.0.0'
+version          '2.1.0'
 
 depends          'osl-firewall'
 depends          'nfs', '~> 5.0.0'
